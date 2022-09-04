39a17542385ea2caf80ac5e3290833c965fb8521
Anis KCHAOU <kchaouanis26@gmail.com>
2022-09-04 18:15:41 +0200

    new changes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'www')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
 app.listen(port, () => console.log("Listening on Port", port)) 
